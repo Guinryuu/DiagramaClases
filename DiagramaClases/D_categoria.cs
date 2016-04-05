b64c92da1b5cd1637b0874eef4af26ad6fca747b
Guinryuu <kilipl.astrape@gmail.com>
2016-04-04 19:21:17 -0500

    Clases Version 1

---
+++ DiagramaClases/D_categoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagramaClases
+{
+    class D_categoria
+    {
+        public int ID_categoria { get; private set; }
+        public string Nombre { get; private set; }
+        public string Descripcion { get; private set; }
+    }
+}
